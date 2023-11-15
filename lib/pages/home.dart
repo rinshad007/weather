f6c40f5a27d0003d5514f6b78cfb46150f243ebd
rinshad007 <150228536+rinshad007@users.noreply.github.com>
2023-11-15 21:20:43 +0530

    Initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Screenhome extends StatefulWidget {
+  const Screenhome({super.key});
+
+  @override
+  State<Screenhome> createState() => _ScreenhomeState();
+}
+
+class _ScreenhomeState extends State<Screenhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
